7bbfe288daceedb1dcd4fb5870b63141503dad8d
lupyuen-unabiz <ly.lee@unabiz.comly.lee@unabiz.com>
2018-04-06 17:11:29 +0800

    Added button presses

--- cmd/scripts/test.sh
+++ cmd/scripts/test.sh
@@ -7,6 +7,8 @@ export GCLOUD_PROJECT=
 # curl -X DELETE https://prometheus-pushgateway-dot-${GCLOUD_PROJECT}.appspot.com/metrics/job/some_job/instance/some_instance
 # curl -X DELETE https://prometheus-pushgateway-dot-${GCLOUD_PROJECT}.appspot.com/metrics/job/some_job
 
-echo "untyped_metric 3.14" | curl --data-binary @- https://prometheus-pushgateway-dot-${GCLOUD_PROJECT}.appspot.com/metrics/job/some_job
+# Untyped Metric
+# echo "untyped_metric 3.14" | curl --data-binary @- https://prometheus-pushgateway-dot-${GCLOUD_PROJECT}.appspot.com/metrics/job/some_job
 
+# Send some metrics
 cat ./scripts/test.txt  | curl --data-binary @- https://prometheus-pushgateway-dot-${GCLOUD_PROJECT}.appspot.com/metrics/job/some_job/instance/some_instance
